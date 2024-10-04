8bc4b1fe5d25f4fd16f3c0e2aaf14587ed1f3d36
edison <yimingy9@illinois.edu>
2024-10-04 01:42:46 -0400

    feat: fix bugs in ArrayIntQueue

--- Java/src/main/java/edu/cmu/cs/cs214/rec02/ArrayIntQueue.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec02/ArrayIntQueue.java
@@ -73,11 +73,14 @@ public class ArrayIntQueue implements IntQueue {
 
     /** {@inheritDoc} */
     public boolean isEmpty() {
-        return size >= 0;
+        return size == 0;
     }
 
     /** {@inheritDoc} */
     public Integer peek() {
+        if (isEmpty()) {
+            return null;
+        }
         return elementData[head];
     }
 
@@ -99,7 +102,7 @@ public class ArrayIntQueue implements IntQueue {
                 newData[i - head] = elementData[i];
             }
             for (int i = 0; i < head; i++) {
-                newData[head - i] = elementData[i];
+                newData[oldCapacity - head + i] = elementData[i];
             }
             elementData = newData;
             head = 0;
