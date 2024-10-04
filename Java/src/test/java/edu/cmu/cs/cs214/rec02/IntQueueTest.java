8f28173895b2e5669c35a99b189c7b8be69785e1
edison <yimingy9@illinois.edu>
2024-10-04 00:52:06 -0400

    feat: finish test cases for LinkedIntQueue

--- Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
+++ Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
@@ -38,8 +38,8 @@ public class IntQueueTest {
     @Before
     public void setUp() {
         // comment/uncomment these lines to test each class
-        mQueue = new LinkedIntQueue();
-    //    mQueue = new ArrayIntQueue();
+//        mQueue = new LinkedIntQueue();
+        mQueue = new ArrayIntQueue();
 
         testList = new ArrayList<>(List.of(1, 2, 3));
     }
@@ -53,19 +53,21 @@ public class IntQueueTest {
     @Test
     public void testNotEmpty() {
         // TODO: write your own unit test
-        fail("Test not implemented");
+        mQueue.enqueue(1);
+        assertFalse(mQueue.isEmpty());
     }
 
     @Test
     public void testPeekEmptyQueue() {
         // TODO: write your own unit test
-        fail("Test not implemented");
+        assertNull(mQueue.peek());
     }
 
     @Test
     public void testPeekNoEmptyQueue() {
         // TODO: write your own unit test
-        fail("Test not implemented");
+        mQueue.enqueue(1);
+        assertEquals(1, mQueue.peek().intValue());
     }
 
     @Test
@@ -73,7 +75,7 @@ public class IntQueueTest {
         // This is an example unit test
         for (int i = 0; i < testList.size(); i++) {
             mQueue.enqueue(testList.get(i));
-            assertEquals(testList.get(0), mQueue.peek());
+            assertEquals(testList.getFirst(), mQueue.peek());
             assertEquals(i + 1, mQueue.size());
         }
     }
@@ -81,7 +83,14 @@ public class IntQueueTest {
     @Test
     public void testDequeue() {
         // TODO: write your own unit test
-        fail("Test not implemented");
+        for (Integer integer : testList) {
+            mQueue.enqueue(integer);
+        }
+        for (int i = 0; i < testList.size(); i++) {
+            Integer element = mQueue.dequeue();
+            assertEquals(testList.get(i), element);
+            assertEquals(testList.size() - i - 1, mQueue.size());
+        }
     }
 
     @Test
